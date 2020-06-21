009f76e3ffc763294ca098786998e3348ed64278
github username <github email address>
2020-06-21 01:06:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Your code here
+function mapToNegativize(sourceArray){
+    let memo =[];
+    for (let i = 0; i < sourceArray.length; i++){
+        memo.push(-1 * sourceArray[i]);
+        }
+        return memo;
+};
+
+function mapToNoChange(sourceArray){
+    let memo =[];
+    for (let i = 0; i < sourceArray.length; i++){
+        memo.push(sourceArray[i]);
+        }
+        return memo;
+};
+
+function mapToDouble(sourceArray){
+    let memo =[];
+    for (let i = 0; i < sourceArray.length; i++){
+        memo.push( 2 * sourceArray[i]);
+        }
+        return memo;
+};
+
+function mapToSquare(sourceArray){
+    let memo =[];
+    for (let i = 0; i < sourceArray.length; i++){
+        memo.push(sourceArray[i] ** 2);
+        }
+        return memo;
+};
+
+function reduceToTotal(sourceArray, startingPoint=0){
+    let total = startingPoint;
+    for (let i = 0; i < sourceArray.length;i++){
+        total += sourceArray[i];
+    };
+return total; 
+};
+
+function reduceToAllTrue(sourceArray){
+    for (let i = 0; i < sourceArray.length; i++){
+    if (!sourceArray[i])
+    return false
+};
+return true;
+};
+
+function reduceToAnyTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i])
+            return true
+    };
+    return false;
+};
